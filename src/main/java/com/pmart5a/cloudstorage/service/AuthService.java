5f5fe4d89d22746a573d8c96df73d320bb767554
Пётр Мартынчик <pmart5a@yandex.ru>
2023-01-14 22:59:44 +0400

    Внесены изменения в класс AuthService: поднятие контекста вынесено в отдельный метод setAuthenticationUser; изменён метод добавления токена в хранилище

--- src/main/java/com/pmart5a/cloudstorage/service/AuthService.java
+++ src/main/java/com/pmart5a/cloudstorage/service/AuthService.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
@@ -24,14 +23,12 @@ public class AuthService {
         final var username = authRequest.getLogin();
         final var password = authRequest.getPassword();
         log.info("Login attempt.");
-        Authentication authentication = authenticationManager.authenticate
-                (new UsernamePasswordAuthenticationToken(username, password));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
+        setAuthenticationUser(username, password);
         final var userAuth = userService.getUserAuth();
         log.info("The user with the ID [{}] has been granted access to the system.", userAuth.getId());
         final var authToken = tokenService.generateToken(userAuth);
         log.info("The user with the ID [{}] has been granted an access token.", userAuth.getId());
-        tokenService.putTokenAndUserLoginInStorage(authToken, userAuth.getLogin());
+        tokenService.addTokenInStorage(authToken);
         log.info("The access token is placed in the storage.");
         return new AuthResponse(authToken);
     }
@@ -42,5 +39,11 @@ public class AuthService {
         log.info("The user with the ID [{}] logged out.", userId);
         tokenService.removeTokenFromStorage(token);
         log.info("The user access token with the ID [{}] removed from storage.", userId);
+    }
+
+    private void setAuthenticationUser(String username, String password) {
+        final var authentication = authenticationManager.authenticate
+                (new UsernamePasswordAuthenticationToken(username, password));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 }
