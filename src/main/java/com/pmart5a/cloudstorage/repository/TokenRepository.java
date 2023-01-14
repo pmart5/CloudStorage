63fea16437b2f6aa6711a473288fb6e17c5d3620
Пётр Мартынчик <pmart5a@yandex.ru>
2023-01-14 23:16:05 +0400

    Внесены изменения в класс TokenRepository: удалён метод getUserLoginByToken; изменены и переименованы методы putTokenAndUserLogin и putAllTokens

--- src/main/java/com/pmart5a/cloudstorage/repository/TokenRepository.java
+++ src/main/java/com/pmart5a/cloudstorage/repository/TokenRepository.java
@@ -1,18 +1,16 @@
 package com.pmart5a.cloudstorage.repository;
 
-import java.util.Set;
+import java.util.List;
 
 public interface TokenRepository {
 
-    void putTokenAndUserLogin(String token, String login);
+    void addToken(String token);
 
     void removeToken(String token);
-
-    String getUserLoginByToken(String token);
 
     boolean isToken(String token);
 
     int getSizeTokens();
 
-    Set<String> putAllTokens();
+    List<String> getAllTokens();
 }
