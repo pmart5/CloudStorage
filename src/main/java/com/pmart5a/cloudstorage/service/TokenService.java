f62d0669f36a57306cc3f729972c8b64d5cb2a07
Пётр Мартынчик <pmart5a@yandex.ru>
2023-01-15 14:28:32 +0400

    Внесены изменения в класс TokenService: внесены изменения в методы; часть методов переименована

--- src/main/java/com/pmart5a/cloudstorage/service/TokenService.java
+++ src/main/java/com/pmart5a/cloudstorage/service/TokenService.java
@@ -8,7 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -18,28 +18,24 @@ public class TokenService {
     private final TokenRepositoryImpl tokenRepository;
     private final TokenUtil tokenUtil;
 
-    public void putTokenAndUserLoginInStorage(String token, String login) {
-        tokenRepository.putTokenAndUserLogin(token, login);
+    public void addTokenInStorage(String token) {
+        tokenRepository.addToken(token);
     }
 
     public void removeTokenFromStorage(String token) {
         tokenRepository.removeToken(token);
     }
 
-    public String getUserLoginFromStorage(String token) {
-        return tokenRepository.getUserLoginByToken(token);
-    }
-
     public boolean isTokenInStorage(String token) {
         return tokenRepository.isToken(token);
     }
 
-    public int getNumberOfRecordsInStorage() {
+    public int getNumberOfTokensInStorage() {
         return tokenRepository.getSizeTokens();
     }
 
-    public Set<String> getAllTokensFromStorage() {
-        return tokenRepository.putAllTokens();
+    public List<String> getAllTokensFromStorage() {
+        return tokenRepository.getAllTokens();
     }
 
     public String generateToken(User user) {
@@ -50,19 +46,24 @@ public class TokenService {
         return tokenUtil.checkToken(token);
     }
 
+    public Long getUserIdFromToken(String token) {
+        return tokenUtil.getUserIdFromToken(token);
+    }
+
     @Scheduled(fixedDelayString = "${jwt.token.check-interval}")
     public void clearTheStorageOfInvalidTokens() {
         log.info("The operation of deleting invalid access tokens from the storage has been started." +
-                " Total tokens in storage: [{}].", getNumberOfRecordsInStorage());
+                " Total tokens in storage: [{}].", getNumberOfTokensInStorage());
         final var tokenAll = getAllTokensFromStorage();
         int numberOfDeletedTokens = 0;
-        if (!tokenAll.isEmpty())
+        if (!tokenAll.isEmpty()) {
             for (final var token : tokenAll) {
                 if (!checkToken(token)) {
                     removeTokenFromStorage(token);
                     numberOfDeletedTokens++;
                 }
             }
+        }
         log.info("The operation of deleting invalid access tokens from the storage has been completed." +
                 " Deleted: [{}].", numberOfDeletedTokens);
     }
