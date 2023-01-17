3c474f470a1ed155113428d91a78ac7375fc4303
Пётр Мартынчик <pmart5a@yandex.ru>
2023-01-17 18:24:02 +0400

    Внесены измения в класс ExceptionHandlerControllerAdviceTest: переменная expectedMessage в методах заменена статическими полями

--- src/test/java/com/pmart5a/cloudstorage/advice/ExceptionHandlerControllerAdviceTest.java
+++ src/test/java/com/pmart5a/cloudstorage/advice/ExceptionHandlerControllerAdviceTest.java
@@ -24,6 +24,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ExceptionHandlerControllerAdviceTest {
 
     private static final String ERROR_MESSAGE = "Error message";
+    private static final String MESSAGE_FILE_NAME_NOT_UNIQUE =
+            "Ошибка ввода данных. Файл с таким именем уже есть в облаке.";
+    private static final String MESSAGE_FILE_NOT_FOUND = "Ошибка ввода данных. Файл с таким именем не найден.";
+    private static final String MESSAGE_NEW_FILE_NAME_UNKNOWN = "Ошибка ввода данных. Отсутствует новое имя файла.";
+    private static final String MESSAGE_NEW_FILE_NAME_NOT_UNIQUE =
+            "Ошибка ввода данных. Новое имя файла совпадает с именем файла в облаке.";
+    private static final String MESSAGE_BAD_CREDENTIALS = "Ошибка ввода данных. Неверные учётные данные.";
+    private static final String MESSAGE_SERVER_ERROR =
+            "Ошибка сервера. Попробуйте повторить операцию через какое-то время.";
 
     private static Stream<Arguments> sourceForHandlerException() {
         return Stream.of(Arguments.of(new AuthenticationServiceException(ERROR_MESSAGE)),
@@ -46,60 +55,53 @@ public class ExceptionHandlerControllerAdviceTest {
     @Test
     public void testHandlerFileNameNotUniqueException() {
         final var exception = new FileNameNotUniqueException(ERROR_MESSAGE);
-        final var expectedMessage = "Ошибка ввода данных. Файл с таким именем уже есть в облаке.";
 
         final var actualResponse = exceptionHandlerControllerAdvice.handlerFileNameNotUniqueException(exception);
 
-        assertEquals(expectedMessage, actualResponse.getMessage());
+        assertEquals(MESSAGE_FILE_NAME_NOT_UNIQUE, actualResponse.getMessage());
     }
 
     @Test
     public void testHandlerFileNotFoundException() {
-        final var expectedMessage = "Ошибка ввода данных. Файл с таким именем не найден.";
         final var exception = new FileNotFoundException(ERROR_MESSAGE);
 
         final var actualResponse = exceptionHandlerControllerAdvice.handlerFileNotFoundException(exception);
 
-        assertEquals(expectedMessage, actualResponse.getMessage());
+        assertEquals(MESSAGE_FILE_NOT_FOUND, actualResponse.getMessage());
     }
 
     @Test
     public void testHandlerNewFileNameUnknownException() {
-        final var expectedMessage = "Ошибка ввода данных. Отсутствует новое имя файла.";
         final var exception = new NewFileNameUnknownException(ERROR_MESSAGE);
 
         final var actualResponse = exceptionHandlerControllerAdvice.handlerNewFileNameUnknownException(exception);
 
-        assertEquals(expectedMessage, actualResponse.getMessage());
+        assertEquals(MESSAGE_NEW_FILE_NAME_UNKNOWN, actualResponse.getMessage());
     }
 
     @Test
     public void testHandlerNewFileNameNotUniqueException() {
-        final var expectedMessage = "Ошибка ввода данных. Новое имя файла совпадает с именем файла в облаке.";
         final var exception = new NewFileNameNotUniqueException(ERROR_MESSAGE);
 
         final var actualResponse = exceptionHandlerControllerAdvice.handlerNewFileNameNotUniqueException(exception);
 
-        assertEquals(expectedMessage, actualResponse.getMessage());
+        assertEquals(MESSAGE_NEW_FILE_NAME_NOT_UNIQUE, actualResponse.getMessage());
     }
 
     @Test
     public void testHandlerAuthenticationException() {
-        final var expectedMessage = "Ошибка ввода данных. Неверные учётные данные.";
         final var exception = new BadCredentialsException(ERROR_MESSAGE);
 
         final var actualResponse = exceptionHandlerControllerAdvice.handlerAuthenticationException(exception);
 
-        assertEquals(expectedMessage, actualResponse.getMessage());
+        assertEquals(MESSAGE_BAD_CREDENTIALS, actualResponse.getMessage());
     }
 
     @ParameterizedTest
     @MethodSource("sourceForHandlerException")
     public void testHandlerException(Exception exception) {
-        final var expectedMessage = "Ошибка сервера. Попробуйте повторить операцию через какое-то время.";
-
         final var actualResponse = exceptionHandlerControllerAdvice.handlerException(exception);
 
-        assertEquals(expectedMessage, actualResponse.getMessage());
+        assertEquals(MESSAGE_SERVER_ERROR, actualResponse.getMessage());
     }
 }
